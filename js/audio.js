72abf2e2c14d907c3cf50f74275d15709b58548f
JoeySnel <9016797@student.zadkine.nl>
2022-05-19 11:25:37 +0200

    Working Game

---
+++ js/audio.js
@@ -0,0 +1,20 @@
+const audio = {
+    map: new Howl({
+        src: 'audio/Mandala Dreams - Paths of a Samurai - trimmed.mp3',
+        volume: 0.04,
+        loop: true
+    }),
+    mapAmbience: new Howl({
+        src: 'audio/forest-birds.wav',
+        volume: 0.2,
+        loop: true
+    }),
+    playerWhoosh: new Howl({
+        src: 'audio/metal-whoosh.wav',
+        volume: 0.1
+    }),
+    kenjiWhoosh: new Howl({
+        src: 'audio/fast-sword-whoosh.wav',
+        volume: 0.1
+    })
+}
